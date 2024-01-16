652855ad0305b14bcd5f47ce2c5aab1c00490882
evlfahim <fahimbinlokman@ymail.com>
2024-01-16 12:54:23 +0400

    new initial commit

---
+++ resources/views/calculator.blade.php
@@ -0,0 +1,8 @@
+<!-- resources/views/calculator.blade.php -->
+
+@extends('layouts.app')
+
+@section('content')
+    <h1>Mortgage Loan Calculator</h1>
+    <!-- Your calculator form and result display here -->
+@endsection
